59bdcbc089c63522c3b081a0c254beaac39b6cb6
Sethbarrie <sethbarrie@gmail.com>
2021-05-02 21:19:46 -0700

    Refactored code to be modular and use callbacks to be able to publish lol

--- spreadsheet_parser/KelseaScript.py
+++ spreadsheet_parser/KelseaScript.py
@@ -5,100 +5,141 @@ import helpers
 
 def main():
 
-    # Tk().withdraw()
-    # input_spreadsheet_path = askopenfilename()
-    # output_spreadsheet_path = askopenfilename()
+    Tk().withdraw()
+    input_spreadsheet_path = askopenfilename()
+    output_spreadsheet_path = askopenfilename()
 
-    # helpers.error_check([
-    #     (input_spreadsheet_path,"Input spreadsheet wasn't selected!"),
-    #     (output_spreadsheet_path,"Output spreadsheet wasn't selected!")
-    # ])
-
-    # wb_input = load_workbook(input_spreadsheet_path)
-    # input_spreadsheet = wb_input.active
-
-    # wb_output = load_workbook(output_spreadsheet_path)
-    # output_spreadsheet = wb_output.active
-
-    input_spreadsheet_path = "./xlsx_files/copy/input.xlsx"
-    output_spreadsheet_path = "./xlsx_files/copy/output_empty.xlsx"
-
+    # Checks if spreadsheet paths are valid
+    helpers.error_check([
+        (input_spreadsheet_path,"Input spreadsheet wasn't selected!", lambda x: x == ""),
+        (output_spreadsheet_path,"Output spreadsheet wasn't selected!", lambda x: x == "")
+    ])
+    
+    # #For testing comment out
+    # input_spreadsheet_path = "./xlsx_files/copy/input.xlsx"
+    # output_spreadsheet_path = "./xlsx_files/copy/output_empty.xlsx"
+    
+    
     wb_input = load_workbook(input_spreadsheet_path)
     input_spreadsheet = wb_input.active
-
     wb_output = load_workbook(output_spreadsheet_path)
     output_spreadsheet = wb_output.active
 
+    #Checks if spreadsheets are valid
     helpers.error_check([
-        (input_spreadsheet, "Input spreadsheet couldn't be read!"),
-        (output_spreadsheet, "Output spreadsheet couldn't be read!")
+        (input_spreadsheet, "Input spreadsheet couldn't be read!", lambda x: not x),
+        (output_spreadsheet, "Output spreadsheet couldn't be read!", lambda x: not x)
+    ])
+    #input spreadsheet columns
+    store_name_column = helpers.term_index_from_tuple(input_spreadsheet['1'], "Store Name")
+    trans_type_column = helpers.term_index_from_tuple(input_spreadsheet['1'], "Trans Type")
+    total_payment_column = helpers.term_index_from_tuple(input_spreadsheet['1'], "Total")
+    
+    #output spreadsheet columns
+    store_number_column = helpers.term_index_from_tuple(output_spreadsheet['1'], "Store Number")
+    cash_column = helpers.term_index_from_tuple(output_spreadsheet['1'], "Cash")
+    credit_column = helpers.term_index_from_tuple(output_spreadsheet['1'], "Credit")
+    cashless_column = helpers.term_index_from_tuple(output_spreadsheet['1'], "Cashless (mobile)")
+
+    #Checks if any necessary column isn't here and throws an error
+    helpers.error_check([
+        (store_name_column, "Input spreadsheet needs a column named 'Store Name'", lambda x: x == -1), 
+        (trans_type_column, "Input spreadsheet needs a column named 'Trans Type'", lambda x: x == -1), 
+        (total_payment_column, "Input spreadsheet needs a column named 'Total'", lambda x: x == -1), 
+
+        (store_number_column, "Output spreadsheet needs a column named 'Store Number'", lambda x: x == -1), 
+        (cash_column, "Output spreadsheet needs a column named 'Cash'", lambda x: x == -1), 
+        (credit_column, "Output spreadsheet needs a column named 'Credit'", lambda x: x == -1), 
+        (cashless_column, "Output spreadsheet needs a column named 'Cashless (mobile)'", lambda x: x == -1)
     ])
 
+    def format_input_dictionary(input_tuple, current_dictionary, row):
+        return_dictionary = {}
+        store_number_string = str(int(input_tuple[store_name_column]))
+        if store_number_string not in current_dictionary:
+            return_dictionary = {"Cash": 0, "Credit": 0, "Cashless (mobile)": 0}
+        else:
+            return_dictionary = current_dictionary[store_number_string]
+        trans_type = input_tuple[trans_type_column]
+        if trans_type != "Cash" and trans_type != "Credit":
+            trans_type = "Cashless (mobile)"
+        return_dictionary[trans_type] += input_tuple[total_payment_column]
 
-    store_number_rows = helpers.create_column_dictionary(output_spreadsheet, "Store Number")
-    store_payment_outputs = helpers.populate_input_types_per_store(input_spreadsheet)
+        return (store_number_string, return_dictionary)
+        
+    def format_output_dictionary(input_tuple, current_dictionary, row):
+        return(input_tuple[store_number_column][13:], row)
+
+    #The dictionaries to work with
+    store_number_rows = helpers.create_rows_dictionary(output_spreadsheet, format_output_dictionary, 2)
+    """
+    store_number_rows = {
+        '101': 2, 
+        '118': 3, 
+        '129': 4, 
+        '132': 5, 
+        '156': 6,
+        ...    
+    }
+    """
+    store_payment_outputs = helpers.create_rows_dictionary(input_spreadsheet, format_input_dictionary)
+    """
+    store_payment_outputs = {
+        '101': {
+            'Cash': 53.6, 
+            'Credit': 8.6, 
+            'Cashless (mobile)': 15.7
+        }, '118': {
+            'Cash': 120.0, 
+            'Credit': 55.6, 
+            'Cashless (mobile)': 7.3
+        }, '129': {
+            'Cash': 40.8, 
+            'Credit': 24.6, 
+            'Cashless (mobile)': 11.8
+        }, '132': {
+            'Cash': 74.4, 
+            'Credit': 19.2, 
+            'Cashless (mobile)': 35.2
+        }, '156': {
+            'Cash': 9.6, 
+            'Credit': 1.8, 
+            'Cashless (mobile)': 0
+        }, ...
+    }
+    """
+    #new_store_payment_outputs will have a similar structure to store_payment_outputs
     new_store_payment_outputs = {}
 
-    store_number_column = helpers.letter_column_from_tuple(output_spreadsheet['1'], "Store Number")
-    cash_column = helpers.letter_column_from_tuple(output_spreadsheet['1'], "Cash")
-    credit_column = helpers.letter_column_from_tuple(output_spreadsheet['1'], "Credit")
-    cashless_column = helpers.letter_column_from_tuple(output_spreadsheet['1'], "Cashless (mobile)")
-
-
-    helpers.error_check([
-        (store_number_column, "Output spreadsheet needs a column named 'Store Number'"), 
-        (cash_column, "Output spreadsheet needs a column named 'Cash'"), 
-        (credit_column, "Output spreadsheet needs a column named 'Credit'"), 
-        (cashless_column, "Output spreadsheet needs a column named 'Cashless (mobile)'")
-    ])
-
+    #Need to define function here for payment_info and store_num scoping
+    def update_payments(row_tuple):
+        total = payment_info["Cash"] + payment_info["Credit"] + payment_info["Cashless (mobile)"]
+        return (
+            ("A", helpers.set_new_value(row_tuple[0], f"Terrible's - {store_num}")),
+            ("B", round(helpers.set_new_value(row_tuple[1], 0)) + payment_info["Cash"], 2),
+            ("C", round(helpers.set_new_value(row_tuple[2], 0)) + payment_info["Credit"], 2),
+            ("D", round(helpers.set_new_value(row_tuple[3], 0)) + payment_info["Cashless (mobile)"], 2),
+            ("E", round(helpers.set_new_value(row_tuple[4], 0)) + total, 2),
+            ("F", round((helpers.set_new_value(row_tuple[4], 0)  + total) / 2.0), 2),
+        )
+    #For stores that are on the output sheet
     for store_num in store_payment_outputs:
-        payment_info = store_payment_outputs[store_num]
         store_row = 0
         if store_num in store_number_rows:
             store_row = str(store_number_rows[store_num])
             
-            helpers.update_number_value_in_spreadsheet(
-                f"{cash_column}{store_row}", 
-                output_spreadsheet, 
-                payment_info["Cash"]
-            )
-            helpers.update_number_value_in_spreadsheet(
-                f"{credit_column}{store_row}", 
-                output_spreadsheet, 
-                payment_info["Credit"]
-            )
-            helpers.update_number_value_in_spreadsheet(
-                f"{cashless_column}{store_row}",
-                output_spreadsheet, 
-                payment_info["Cashless (mobile)"]
-            )
+            payment_info = store_payment_outputs[store_num]
+            helpers.manipulate_row(output_spreadsheet, update_payments, store_row)
         else:
             new_store_payment_outputs[store_num] = store_payment_outputs[store_num]
-
+    #For stores that need to be inserted on the output sheet
     for store_num in new_store_payment_outputs:
         payment_info = store_payment_outputs[store_num]
-
-        helpers.insert_values_into_spreadsheet(
-            [store_number_column, cash_column, credit_column, cashless_column],
-            output_spreadsheet, 
-            payment_info,
-            int(store_num)
-        )
-    def total_function(spreadsheet, row):
-        print("In total function")
-        for cell in spreadsheet[row]:
-            print(cell.value)
-        val1 = int(spreadsheet["B" + str(row)].value)
-        val2 = int(spreadsheet["C" + str(row)].value)
-        val3 = int(spreadsheet["D" + str(row)].value)
-        new_total = 0 if spreadsheet["E" + str(row)].value is None else int(spreadsheet["E" + str(row)].value)
-        spreadsheet["E" + str(row)] = new_total + (val1 + val2 + val3)
-        spreadsheet["F" + str(row)] = (int(spreadsheet["E" + str(row)].value)/ 2)
-
-    helpers.enumerate_rows(output_spreadsheet, total_function, 2)
+        store_row = helpers.insert_row(output_spreadsheet, "A", int(store_num),lambda x: int(x[13:]) ,2)
+        helpers.manipulate_row(output_spreadsheet, update_payments, store_row)
+    #Save your progress!
     wb_input.save(input_spreadsheet_path)
     wb_output.save(output_spreadsheet_path)
-    
+
 if __name__ == "__main__":
     main()
--- spreadsheet_parser/helpers.py
+++ spreadsheet_parser/helpers.py
@@ -2,23 +2,13 @@ import sys
 from openpyxl import load_workbook
 ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def populate_store_numbers(spreadsheet, starting_row = 2):
-    output = {}
-    column = letter_column_from_tuple(spreadsheet['1'], "Store Number")
-    counter = starting_row
-    cell_name = column + str(counter)
-    while spreadsheet[cell_name].value != None:
-        store_num_string = spreadsheet[cell_name].value
-        output[store_num_string[13:]] = counter
-        counter += 1
-        cell_name = column + str(counter)
-    return output
+
 """ 
 Creates dictionary from specified column, 
 requires: 
     spreadsheet, 
     column name not letter,
-    *optional* starting row.
+    *optional* starting row, defaults to 2, since openpyxl is 1 indexed.
 Iterates row by row until it doesn't find a value. Creates dictionary
 with the cell value as the key and the row as the value
 """
@@ -34,30 +24,17 @@ def create_column_dictionary(spreadsheet, name, starting_row = 2):
         cell_name = column + str(counter)
     return output
 
-def populate_input_types_per_store(spreadsheet):
-    output = {}
-    store_name = term_index_from_tuple(spreadsheet['1'], "Store Name")
-    trans_type = term_index_from_tuple(spreadsheet['1'], "Trans Type")
-    payment_total = term_index_from_tuple(spreadsheet['1'], "Total")
+def create_rows_dictionary(spreadsheet, callback, starting_row = 2):
+    return_dictionary = {}
 
-    end_of_spreadsheet = False
-    counter = 1
-    while not end_of_spreadsheet:
-        counter += 1
-        row = spreadsheet[counter]
-        end_of_spreadsheet = any(map(lambda x: x.value is None, row))
-        if end_of_spreadsheet or row[0] == "Device":
-            break
-        store_num = str(int(row[store_name].value))
-        if store_num not in output:
-            new_cash_dictionary = {"Cash": 0, "Credit": 0, "Cashless (mobile)": 0}
-            output[store_num] = new_cash_dictionary
-        payment_type = row[trans_type].value
-        if payment_type != "Cash" and payment_type != "Credit":
-            payment_type = "Cashless (mobile)"
-        new_total = float(row[payment_total].value) + output[store_num][payment_type]
-        output[store_num][payment_type] = round(new_total, 2)
-    return output
+    current_row = starting_row
+    end_of_rows = end_of_spreadsheet(spreadsheet[current_row])
+    while not end_of_rows:
+        kv_tuple = callback(create_value_tuple(spreadsheet[current_row]), return_dictionary, current_row)
+        return_dictionary[kv_tuple[0]] = kv_tuple[1]
+        current_row += 1
+        end_of_rows = end_of_spreadsheet(spreadsheet[current_row])
+    return return_dictionary
 
 """
 Returns the index from the tuple of the specific value, 
@@ -73,64 +50,46 @@ def term_index_from_tuple(header, term):
 Returns the column letter of the specific value
 """
 def letter_column_from_tuple(header, term):
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     counter = term_index_from_tuple(header, term)
-    return alpha[counter]
+    return ALPHA[counter]
 
-"""
-If cell is empty, sets value to value,
-if cell is a number, adds value to previous value
-"""
-def update_number_value_in_spreadsheet(cell, output_spreadsheet, value):
-    cell_value = output_spreadsheet[cell].value
-    output = cell_value if cell_value != None else 0
-    output_spreadsheet[cell] = float(output) + float(value)
-
-
-def insert_values_into_spreadsheet(column_array, output_spreadsheet, payment_info, store_num):
-    current_row = 2
-    previous_store_number = 0
-    found_insert_spot = output_spreadsheet[column_array[0] + str(current_row)].value != None
-    while found_insert_spot:
-        current_store_number = int(output_spreadsheet[column_array[0] + str(current_row)].value[13:])
-        if current_store_number > store_num and store_num > previous_store_number:
-            found_insert_spot = false
-        else:
-            previous_store_number = current_store_number
-            current_row += 1
-            found_insert_spot = output_spreadsheet[column_array[0] + str(current_row)].value != None
-
-    output_spreadsheet.insert_rows(current_row)
-    output_spreadsheet[f"{column_array[0]}{current_row}"] = f"Terrible's - {store_num}"
-    output_spreadsheet[f"{column_array[1]}{current_row}"] = float(payment_info["Cash"])
-    output_spreadsheet[f"{column_array[2]}{current_row}"] = float(payment_info["Credit"])
-    output_spreadsheet[f"{column_array[3]}{current_row}"] = float(payment_info["Cashless (mobile)"])
 
 def error_check(array):
+    """
+    array = [
+        (<Value>,<Error message if failed>, <Callback to test value>),
+        (1, "Output spreadsheet needs a column named 'Example'", lambda x: x == -1),
+        ...
+    ]
+    """
     for error_container in array:
-        if not error_container[0]:
+        if error_container[2](error_container[0]):
             print(error_container[1])
             sys.exit(1)
 
 
 def manipulate_rows_by_column(spreadsheet, callback, column, starting_row = 1):
     current_row_number = starting_row
-    current_cell = spreadsheet[column + str(current_row_number)]
+    cell_string = f"{column}{current_row_number}"
+    current_cell = spreadsheet[cell_string]
     while not current_cell:
-        spreadsheet[column + str(current_row_number)] = callback(current_cell)
+        spreadsheet[cell_string] = callback(current_cell)
         current_row_number += 1
-        current_row_tuple = spreadsheet[column + str(current_row_number)]
+        cell_string = f"{column}{current_row_number}"
+        current_row_tuple = spreadsheet[cell_string]
 
 def manipulate_columns_by_row(spreadsheet, callback, row, starting_column = "A"):
     current_column_letter = starting_column
     current_cell = spreadsheet[current_column_letter + str(row)]
+    cell_string = f"{current_column_letter}{row}"
     while not current_cell:
-        spreadsheet[current_column_letter + str(row)] = callback(current_cell)
+        spreadsheet[cell_string] = callback(current_cell)
         if current_column_letter == "Z":
             current_cell = false
         else:
             current_column_letter = ALPHA[(ALPHA.index(current_column_letter) + 1)]
-            current_row_tuple = spreadsheet[current_column_letter + str(row)]
+            cell_string = f"{current_column_letter}{row}"
+            current_row_tuple = spreadsheet[cell_string]
 
 def enumerate_rows(spreadsheet, callback, starting_row = 1):
     current_row_number = starting_row
@@ -140,7 +99,56 @@ def enumerate_rows(spreadsheet, callback, starting_row = 1):
         callback(spreadsheet, current_row_number)
         current_row_number += 1
         end_of_rows = end_of_spreadsheet(spreadsheet[current_row_number])
-    
 
+#Since all the tuples return cells, I created this so I can just have values from the cells
+def create_value_tuple(cell_tuple):
+    mid_array = []
+    for cell in cell_tuple:
+        mid_array.append(cell.value)
+    return tuple(mid_array)
+
+#Considered having this as any, but if there were missing values it wouldn't go to the end of the spreadsheet
 def end_of_spreadsheet(row_tuple):
     return all(map(lambda x: x.value is None, row_tuple))
+
+#Runs a callback on the current row, then inserts values from returned tuple
+def manipulate_row(spreadsheet, callback, row):
+    """
+    insert_tuple = (
+            (<Column letter to insert at>, <Value to insert in that cell>),
+            ("A", "Store name 50"),
+            ("B", 25),
+            ("C", 125),
+            ("D", 55.3),
+            ("E", 5),
+            ("F", 0.05),
+        )
+    """
+    insert_tuple = callback(create_value_tuple(spreadsheet[row]))
+    for val in insert_tuple:
+        spreadsheet[f"{val[0]}{str(row)}"] = val[1]
+
+#If there is an ordered column, find and insert an empty row where it would be sorted
+def insert_row(spreadsheet, insert_column, insert_value, callback, starting_row = 2):
+    current_row = starting_row
+    lower_insert_value = 0
+    cell_string = f"{insert_column}{current_row}"
+    found_insert_spot = spreadsheet[cell_string].value != None
+    while found_insert_spot:
+        current_value = callback(spreadsheet[cell_string].value)
+        if current_value > insert_value and insert_value > lower_insert_value:
+            found_insert_spot = false
+        else:
+            lower_insert_value = current_value
+            current_row += 1
+            cell_string = f"{insert_column}{current_row}"
+            found_insert_spot = spreadsheet[cell_string].value != None
+    spreadsheet.insert_rows(current_row)
+    return current_row
+
+#Returns either cells value or default value if cell was empty
+def set_new_value(check_value, new_value):
+    if check_value == None:
+        return new_value
+    else:
+        return check_value
